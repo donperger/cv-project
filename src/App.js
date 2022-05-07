0cb88127c73d1c90de63fa4ce2554b0df0194a00
Rafael Perger <rafael.perger@gmail.com>
2022-05-07 10:33:50 +0200

    Turn to class component. Generate basci info

--- src/App.js
+++ src/App.js
@@ -1,11 +1,44 @@
 import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      Hello from App!
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      generalInfo: {
+        name: 'Jacob Gipsz',
+        email: 'jack.gipsz@example.com',
+        phoneNumber: '+36234568945'
+      },
+      eduExp: [{
+        schoolName: 'University of Technology',
+        titleOfStudy: 'Computer science',
+        dateOfStudy: {
+          start: new Date(2016, 9),
+          end: new Date(2020, 6)
+        }
+      }],
+      practicalExp: [{
+        companyName: 'IT Giants',
+        position: 'Junior web developer',
+        mainTask: 'Create new web applications',
+        date: {
+          start: new Date(2020, 8),
+          end: new Date()
+        }
+      }]
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        Hello from App!
+      </div>
+    );
+  }
 }
+
 
 export default App;

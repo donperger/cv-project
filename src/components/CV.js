c8c489f0b17678dc33516dff6cf49048beb83bd8
Rafael Perger <rafael.perger@gmail.com>
2022-05-08 12:11:25 +0200

    Update component

--- src/components/CV.js
+++ src/components/CV.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import editIcon from '../images/edit-svgrepo-com.svg';
 import '../styles/CV.css';
+import format from 'date-fns/format';
 
 class Cv extends Component {
   constructor(props) {
@@ -21,6 +22,29 @@ class Cv extends Component {
             return (
               <li key={info.id}>
                 {info.fieldName}: {info.fieldValue}
+              </li>
+            );
+          })}
+        </div>
+
+        <div className="edu-exp-info">
+          <ul>
+            Education
+            <button>
+              <img className="edit-img" src={editIcon}></img>
+            </button>
+          </ul>
+          {this.props.eduInfo.map((info) => {
+            return (
+              <li key={info.id}>
+                <div className="school-name">{info.schoolName}</div>
+                <div className="school-info">
+                  <span>{info.titleOfStudy}</span>
+                  <span>
+                    {format(info.dateOfStudy.start, 'MMMM, y')} -
+                    {format(info.dateOfStudy.end, 'MMMM, y')}
+                  </span>
+                </div>
               </li>
             );
           })}

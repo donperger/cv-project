2ba87eb4d507ed73939c11d1536c7f4b84f7012b
Rafael Perger <rafael.perger@gmail.com>
2022-05-08 11:28:43 +0200

    Implement editi and submit of general inforamtion

--- src/components/GeneralInfo.js
+++ src/components/GeneralInfo.js
@@ -1,38 +1,84 @@
-/* eslint-disable require-jsdoc */
 import React, { Component } from 'react';
 import '../styles/GeneralInfo.css';
 
 class GeneralInfo extends Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      name: this.props.name.fieldValue,
+      email: this.props.email.fieldValue,
+      phone: this.props.phoneNum.fieldValue,
+    };
   }
 
+  handleChange = (e) => {
+    if (e.target.id === 'name') {
+      this.setState({ name: e.target.value });
+    } else if (e.target.id === 'email') {
+      this.setState({
+        email: e.target.value,
+      });
+      console.log(e.target.value);
+    } else if (e.target.id === 'phone') {
+      this.setState({
+        phone: e.target.value,
+      });
+    }
+  };
+
+  updateInfo = (e) => {
+    e.preventDefault();
+    this.props.updateGenInfo(
+      this.state.name,
+      this.state.email,
+      this.state.phone
+    );
+  };
+
   render() {
+    const { name, email, phoneNum } = this.props;
+
     return (
-      <form className="general-info-form">
+      <form className="general-info-form" onSubmit={this.updateInfo}>
         <legend>General information</legend>
         <div className="name-cont">
           <label>
             Name
-            <input type="text" id="name" />
+            <input
+              type="text"
+              id="name"
+              defaultValue={name.fieldValue}
+              onChange={this.handleChange}
+            />
           </label>
         </div>
 
         <div className="email-cont">
           <label>
             E-mail
-            <input type="text" id="email" />
+            <input
+              type="text"
+              id="email"
+              defaultValue={email.fieldValue}
+              onChange={this.handleChange}
+            />
           </label>
         </div>
 
         <div className="phone-cont">
           <label>
             Phone Number
-            <input type="text" id="phone" />
+            <input
+              type="text"
+              id="phone"
+              defaultValue={phoneNum.fieldValue}
+              onChange={this.handleChange}
+            />
           </label>
         </div>
 
-        <button>Add info</button>
+        <button>Add</button>
       </form>
     );
   }
